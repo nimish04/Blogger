eb1c087c09c5786ef199824007b289686464f05b
Nimish Mangal <nimish.mangal910@gmail.com>
2018-05-21 15:58:36 +0530

    Inside Django project

--- urls.py
+++ urls.py
@@ -1,18 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 
-from .views import (
-	post_list,
-	post_create,
-	post_detail,
-	post_update,
-	post_delete,
-	)
-
 urlpatterns = [
-	url(r'^$', post_list,name='list'),
-    url(r'^create/$', post_create),
-    url(r'^(?P<id>\d+)/$', post_detail,name='detail'),
-    url(r'^(?P<id>\d+)/edit/$', post_update,name='update'),
-    url(r'^(?P<id>\d+)/delete/$', post_delete),
-    ]
+    url(r'^admin/', admin.site.urls),
+    url(r'^posts/', include("posts.urls",namespace='posts')),
+]
